4421e6533b4007790298355a0968ba05ccc26991
Claudio Henrique <HacskBoy@hotmail.com>
2021-05-21 22:11:56 -0300

    feat: increments navigation view with bottom navigation

---
+++ app/src/main/java/com/dispositivos/moveis/listee/InspirationsActivity.java
@@ -0,0 +1,18 @@
+package com.dispositivos.moveis.listee;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class InspirationsActivity extends Fragment {
+
+    @Nullable
+    public View onCreateView(LayoutInflater layoutInflater,@NonNull ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return layoutInflater.inflate(R.layout.activity_inspirations, container, false);
+    }
+}
--- app/src/main/java/com/dispositivos/moveis/listee/MainActivity.java
+++ app/src/main/java/com/dispositivos/moveis/listee/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dispositivos.moveis.listee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeActivity()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.menu_home:
+                            fragment = new HomeActivity();
+                            break;
+                        case R.id.menu_inspirations:
+                            fragment = new InspirationsActivity();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                    return true;
+                }
+            };
 }
