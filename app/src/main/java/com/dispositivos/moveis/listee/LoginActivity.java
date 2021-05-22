fdfb28f3c8030886fc3f1c004e549b78119f1fbb
Mateus Gomes <mateus.gs1999@gmail.com>
2021-05-22 19:00:15 -0300

    Classes das Activitys Login e Register adicionados

--- app/src/main/java/com/dispositivos/moveis/listee/LoginActivity.java
+++ app/src/main/java/com/dispositivos/moveis/listee/LoginActivity.java
@@ -2,7 +2,11 @@ package com.dispositivos.moveis.listee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.buttom_login);
+        TextView linkCadastro = findViewById(R.id.login_not_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        linkCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/dispositivos/moveis/listee/RegisterActivity.java
+++ app/src/main/java/com/dispositivos/moveis/listee/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.dispositivos.moveis.listee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        FloatingActionButton btnBack = findViewById(R.id.register_buttom_back);
+        Button btnRegister = findViewById(R.id.buttom_register);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
