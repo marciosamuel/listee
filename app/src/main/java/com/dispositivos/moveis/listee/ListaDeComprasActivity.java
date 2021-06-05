6f466960ba04ea06fcef8a1496c7f6271b6d4f7d
marciosamuel <marciosamuel12@outlook.com>
2021-06-05 17:20:00 -0300

    associação de ids das views nas activities

--- app/src/main/java/com/dispositivos/moveis/listee/ListaDeComprasActivity.java
+++ app/src/main/java/com/dispositivos/moveis/listee/ListaDeComprasActivity.java
@@ -1,9 +1,15 @@
 package com.dispositivos.moveis.listee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListaDeComprasActivity extends AppCompatActivity {
 
@@ -12,7 +18,20 @@ public class ListaDeComprasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_compras);
 
-        ListView listaDeProdutos = findViewById(R.id.lista_compras_listView);
+        ImageView imagemTopo = findViewById(R.id.lista_compras_imageView);
+        TextView nomeLista = findViewById(R.id.lista_compras_textView);
+        ListView listaProdutos = findViewById(R.id.lista_compras_listView);
+        Button adicionarProduto = findViewById(R.id.lista_compras_Button);
 
+        Produto produto = new Produto();
+
+        produto.setContainer(findViewById(R.id.produto_cardview));
+        produto.setNome(findViewById(R.id.produto_nome));
+        produto.setImagem(findViewById(R.id.produto_image));
+        produto.setQuantidade(findViewById(R.id.produto_quantidade_valor));
+        produto.setUnidade(findViewById(R.id.produto_unidade));
+        produto.setAumentarQuantidade(findViewById(R.id.produto_botao_aumentar));
+        produto.setDiminuirQuantidade(findViewById(R.id.produto_botao_diminuir));
+        produto.setRemoverProduto(findViewById(R.id.produto_botao_remover));
     }
 }
--- app/src/main/java/com/dispositivos/moveis/listee/StockActivity.java
+++ app/src/main/java/com/dispositivos/moveis/listee/StockActivity.java
@@ -3,18 +3,44 @@ package com.dispositivos.moveis.listee;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StockActivity extends Fragment {
 
     @Nullable
-    public View onCreateView(LayoutInflater layoutInflater, @NonNull ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return layoutInflater.inflate(R.layout.activity_stock, container, false);
+    public View onCreateView(LayoutInflater layoutInflater,@NonNull ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = layoutInflater.inflate(R.layout.activity_stock, container, false);
 
+        ListView listaProdutos = view.findViewById(R.id.estoque_listView);
+        Button adicionarProduto = view.findViewById(R.id.estoque_Button);
+
+        Produto produto = new Produto();
+
+        produto.setContainer(view.findViewById(R.id.produto_cardview));
+        produto.setNome(view.findViewById(R.id.produto_nome));
+        produto.setImagem(view.findViewById(R.id.produto_image));
+        produto.setQuantidade(view.findViewById(R.id.produto_quantidade_valor));
+        produto.setUnidade(view.findViewById(R.id.produto_unidade));
+        produto.setAumentarQuantidade(view.findViewById(R.id.produto_botao_aumentar));
+        produto.setDiminuirQuantidade(view.findViewById(R.id.produto_botao_diminuir));
+        produto.setRemoverProduto(view.findViewById(R.id.produto_botao_remover));
+
+        return view;
     }
 }
