80a2a98888c9c6e06ba5d29fcd57ecc395f4074b
Mateus Gomes <mateus.gs1999@gmail.com>
2021-06-29 13:34:04 -0300

    adicionado id e tirado senha do modal

--- app/src/main/java/Models/UserModel.java
+++ app/src/main/java/Models/UserModel.java
@@ -1,15 +1,19 @@
 package Models;
 
 public class UserModel {
+    private String id;
     private String user;
     private String email;
-    private String password;
+//    private String password;
 
-    public UserModel(String user, String email, String password){
+    public UserModel(String id, String user, String email){
+        this.id = id;
         this.user = user;
         this.email = email;
-        this.password = password;
+//        this.password = password;
     }
+
+    public String getId() { return id; }
 
     public String getUser() {
         return user;
@@ -19,15 +23,5 @@ public class UserModel {
         return email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//    public String getPassword() { return password; }
 }
