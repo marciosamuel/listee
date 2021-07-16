c327139e6635f76a1bb74cc37cbe2ece67f525b4
Claudio Henrique <HacskBoy@hotmail.com>
2021-07-15 22:52:21 -0300

    refactor: increments new attributes

--- app/src/main/java/Models/HomeCardModel.java
+++ app/src/main/java/Models/HomeCardModel.java
@@ -1,14 +1,35 @@
 package Models;
 
 public class HomeCardModel {
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    private String id;
+    private String user_id;
     private String title;
+    private String subTitle;
     private String selectedItems;
     private String remainingItems;
 
-    public HomeCardModel(String title, String selectedItems, String remainingItems){
+    public HomeCardModel(String user_id, String title, String subTitle, String selectedItems, String remainingItems){
+        this.user_id = user_id;
         this.title = title;
+        this.subTitle = subTitle;
         this.selectedItems = selectedItems;
         this.remainingItems = remainingItems;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getTitle() {
