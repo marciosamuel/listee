fe52272abd8e0821e7d4db7a150aa6ae7bd5cde1
marciosamuel <marciosamuel12@outlook.com>
2021-07-28 22:22:42 -0300

    Adicionando intent para edição

--- app/src/main/java/com/dispositivos/moveis/listee/ListaDeComprasActivity.java
+++ app/src/main/java/com/dispositivos/moveis/listee/ListaDeComprasActivity.java
@@ -3,8 +3,12 @@ package com.dispositivos.moveis.listee;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,7 +21,11 @@ import Adapters.ProdutoAdapter;
 import Models.ProdutoModel;
 
 public class ListaDeComprasActivity extends AppCompatActivity {
-    private static ProdutoAdapter adapter;
+
+
+    String nome = "Lista de teste";
+    String descricao = "Lista feita apenas para testar o funcionamento e verificar se está tudo certo";
+    boolean isInspiration = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +35,30 @@ public class ListaDeComprasActivity extends AppCompatActivity {
         List<ProdutoModel> produtos = getProdutos();
         ListView listaDeProdutos = (ListView) findViewById(R.id.sale_list_products);
 
-        adapter = new ProdutoAdapter(this, produtos);
+        TextView name = findViewById(R.id.sale_list_header_text);
+        name.setText(nome);
+        TextView description = findViewById(R.id.sale_list_description);
+        description.setText(descricao);
+
+        ProdutoAdapter adapter = new ProdutoAdapter(this, produtos);
         listaDeProdutos.setAdapter(adapter);
 
         Bundle data = getIntent().getExtras();
         String idProduct = data.getString("ID_PRODUCT");
+
+        Context context = this;
+
+        ImageButton editBtn = findViewById(R.id.sale_list_btn_edit);
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, EditListActivity.class);
+                i.putExtra("LIST_NAME", nome);
+                i.putExtra("LIST_DESCRIPTION", descricao);
+                i.putExtra("LIST_INSPIRATION", isInspiration);
+                startActivity(i);
+            }
+        });
     }
 
     protected List<ProdutoModel> getProdutos(){
