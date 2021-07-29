e5b19cd6c173d33a93b787a31728547cbebd19d3
Mateus Gomes <mateus.gs1999@gmail.com>
2021-07-28 23:10:21 -0300

    Adapter do produto com checkbox dentro da lista

--- app/src/main/java/Adapters/ProductListAdapter.java
+++ app/src/main/java/Adapters/ProductListAdapter.java
@@ -1,15 +1,22 @@
 package Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 
+import com.dispositivos.moveis.listee.ListaDeComprasActivity;
 import com.dispositivos.moveis.listee.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.List;
 
@@ -22,10 +29,14 @@ public class ProductListAdapter extends ArrayAdapter<ProdutoModel> {
     public static class ViewHolder{
         TextView nome;
         TextView quantidade;
+        CheckBox checkboxProduct;
+        CardView cardProduct;
 
         public ViewHolder(View view){
+            cardProduct = (CardView) view.findViewById(R.id.produto_cardview);
             nome = (TextView) view.findViewById(R.id.product_name);
             quantidade = (TextView) view.findViewById(R.id.product_quantity);
+            checkboxProduct = (CheckBox) view.findViewById(R.id.checkbox_product);
         }
     }
 
@@ -57,7 +68,7 @@ public class ProductListAdapter extends ArrayAdapter<ProdutoModel> {
         if(convertView == null){
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-            convertView = inflater.inflate(R.layout.produto, parent, false);
+            convertView = inflater.inflate(R.layout.product_list, parent, false);
             holder = new ProductListAdapter.ViewHolder(convertView);
 
             convertView.setTag(holder);
@@ -68,6 +79,32 @@ public class ProductListAdapter extends ArrayAdapter<ProdutoModel> {
         ProdutoModel produto = getItem(position);
         holder.nome.setText(produto.getNome());
         holder.quantidade.setText(produto.getQuantidade());
+
+        holder.checkboxProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(holder.checkboxProduct.isChecked()) {
+                    holder.nome.setTextColor(Color.parseColor("#C3C3C3"));
+                } else {
+                    holder.nome.setTextColor(Color.parseColor("#FF5789"));
+                }
+            }
+        });
+
+        holder.cardProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext(), R.style.popupInspirationTheme);
+                View popupEditProduct = LayoutInflater.from(getContext()).inflate(R.layout.activity_popup_edit_product, v.findViewById(R.id.popup_edit_product_container));
+
+                EditText newProductName = v.findViewById(R.id.edit_product_name);
+                EditText newProductQuantity = v.findViewById(R.id.edit_product_quantity);
+                Button addProduct = v.findViewById(R.id.btn_edit_product);
+
+                bottomSheetDialog.setContentView(popupEditProduct);
+                bottomSheetDialog.show();
+            }
+        });
 
         return convertView;
     }
